d8f70c6a1d79c07afc4c6dfce9b2be6063f92db3
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-07-10 11:48:39 -0500

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,16 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    # Your code here   
+    cache ={}
+    #loop through the weights
+    for i in range(len(weights)):
+        #store the values
+        cache[weights[i]] = i
+    for i in range(len(weights)):
+        diff = limit-weights[i]
+        if diff in cache:
+            return (max(i,cache[diff]), min(i, cache[diff]))       
     return None
+
+#get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40],6,7)
