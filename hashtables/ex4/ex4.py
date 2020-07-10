f74dc34e4d194a432bb85ac30cfb0ea1e9dc8b21
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-07-10 12:42:06 -0500

    MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,30 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    #traverse the array
+    pairs = set()
+    #indicator to see if we found pair
+    found_pair = False
+    #store positive numbers
+    for i in range(len(a)):
+        #print(a[i])
+        #if num is > 0 store it in set
+        if a[i] > 0:
+            pairs.add(a[i])
+            #print(pairs)
+    #go through the array
+    for i in range(len(a)):
+        #check to see if val of curr is in set
+        if a[i] < 0:
+            #then if that - value is in set
+            if(-a[i]) in pairs:
+                #we return the pair
+                #print("{}, {}".format(a[i], -a[i]))
+
+
+
+
 
     return result
 
